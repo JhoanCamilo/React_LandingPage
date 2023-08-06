807ce7eafc40a6f1dd0920c4c4ed5ceeffaa066e
jcarango <jcamonsalve01@gmail.com>
2023-08-06 15:09:09 -0500

    Formulario terminado, lista la página para revisión

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -4,7 +4,7 @@ export function Carousel() {
     if (screen.width >= 768) {
         return(
             <section className='container-fluid'>
-                <h3 className={Styles.carouselTitle}>De tu <strong className={`${Styles.strongText}`}>idea</strong> a la <strong className={`${Styles.strongText}`}>realidad</strong></h3>
+                <h3 className={Styles.carouselTitle}>De la <strong className={`${Styles.strongText}`}>idea</strong> a la <strong className={`${Styles.strongText}`}>realidad</strong></h3>
                 <p className='text-center'>Si tienes una idea para promocionar tu marca con una web, pero no sabes cómo hacerlo, contacta con nosotros y te ayudaremos a <strong className={`${Styles.strongText}`}>llevar tu negocio al siguiente nivel</strong>.</p>
                 <div className={`${Styles.carouselContainer}`}>
                     <img src={idea} className={`${Styles.carouselImage}`} alt="from idea"/>
@@ -15,7 +15,7 @@ export function Carousel() {
     if (screen.width <= 767){
         return(
             <section className='container-fluid'>
-                <h3 className={Styles.carouselTitle}>De tu <strong className={`${Styles.strongText}`}>idea</strong> a la <strong className={`${Styles.strongText}`}>realidad</strong></h3>
+                <h3 className={Styles.carouselTitle}>De la <strong className={`${Styles.strongText}`}>idea</strong> a la <strong className={`${Styles.strongText}`}>realidad</strong></h3>
                 <p className='text-center'>Si tienes una idea para promocionar tu marca con una web, pero no sabes cómo hacerlo, contacta con nosotros y te ayudaremos a <strong className={`${Styles.strongText}`}>llevar tu negocio al siguiente nivel</strong>.</p>
                 <div className={`d-block ${Styles.carouselContainer}`}>
                     <img src={idea} className={`${Styles.carouselImage}`} alt="from idea"/>
--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,27 +1,67 @@
+import { useRef } from 'react'
 import Style from './Form.module.css'
+import './librariesStyles.css'
 import {IoIosSend} from 'react-icons/io'
-export function ContactForm() {
+import emailjs from '@emailjs/browser'
+import Swal from 'sweetalert2'
+
+
+export function ContactForm(){
+    const form = useRef();
+    const userName = useRef();
+    const userEmail = useRef();
+    const userProject = useRef();
+
+    function sendMail(event) {
+        event.preventDefault()
+
+        const serviceID = 'service_0cd9ats'
+        const templateID = 'template_9j793zd'
+        const publicKey = 'dQe9zo4wqnUVMDdj7'
+        
+        emailjs.sendForm(serviceID, templateID, form.current, publicKey)
+            .then((result) => {
+                console.log(result.text);
+                Swal.fire({
+                    title: 'Genial!',
+                    text: 'Nos comunicaremos contigo en breve',
+                    icon: 'success',
+                    confirmButtonText: 'Aceptar'
+                })
+            }, (err) => {
+                Swal.fire({
+                    title: 'Oops!',
+                    text: 'Algo salió mal, lo solucionaremos pronto.',
+                    icon: 'error',
+                    confirmButtonText: 'Aceptar'
+                })
+            })
+
+        userName.current.value = '';
+        userEmail.current.value = '';
+        userProject.current.value = '';
+    }
     return(
         <section className={`container-fluid ${Style.formContainer}`}>
             <h1 className={`${Style.contactTitle}`}>Hagamos <strong className={`${Style.strongText}`}>algo grandioso</strong></h1>
-            <form action="" className={`container ${Style.formContainer}`}>
+            <form action="" className={`container ${Style.formContainer}`} ref={form} onSubmit={sendMail}>
                 <div className={`${Style.basicInfo}`}>
                     <div className="col-12">
                         <label htmlFor="userName" className="form-label">Tu nombre o el de tu negocio:</label>
-                        <input type="text" className="form-control" id="userName"/>
+                        <input type="text" className="form-control" id="userName" name='userName' ref={userName}/>
                     </div>
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="userEmail" className="form-label">Dirección de Email:</label>
-                    <input type="email" className="form-control" id="userEmail" placeholder="name@example.com"/>
+                    <label htmlFor="userEmail" className="form-label">Correo electrónico:</label>
+                    <input type="email" className="form-control" id="userEmail" placeholder="name@example.com" name='userEmail' ref={userEmail}/>
                     <div id="emailHelp" className="form-text">No compartiremos tu correo con nadie más.</div>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="userProject" className="form-label">Cuentanos sobre tu proyecto</label>
-                    <div id="emailHelp" className="form-text">Danos una <strong>breve descripción</strong> del proyecto que tienes en mente.</div>
-                    <textarea className="form-control" id="userProject" rows="3"></textarea>
+                    <div id="ProjectDesc" className="form-text">Danos una <strong>breve descripción</strong> del proyecto que tienes en mente.</div>
+                    <textarea className="form-control" id="userProject" rows="3" name='userProject' ref={userProject}></textarea>
                 </div>
-                <button className={`btn ${Style.sendBtn}`}>Enviar <IoIosSend/></button>
+                <button className={`btn ${Style.sendBtn}`} id='SendButton' type='submit'>Enviar <IoIosSend/></button>
             </form>
         </section>
     )
